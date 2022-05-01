329981ed778b358349cd31757e31bc4cb32ed2b3
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-01 17:33:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const mySecret = process.env['BOT_TOKEN']
+// index.js
+
+// imports the node package for us
+const TelegramBot = require('node-telegram-bot-api');
+
+// grabs the environment variable
+const token = process.env.BOT_TOKEN;
+
+// starts a new Telegram bot instance that "polls" for updates
+const bot = new TelegramBot(token, { polling: true });
+
+// index.js
+
+// ... after everything we wrote already
+
+// we're using the API's in-built "onText" method
+// it takes in a regex and the message
+bot.onText(/\/start/, (msg) => {
+// listens for "/start" and responds with the greeting below.
+bot.sendMessage(msg.chat.id,
+"Hey, I'm a brand new Telegram bot. I live inside a Sanity tutorial.");
+});
+
+// index.js
+
+// ... at the bottom
+bot.on('message', (msg) => {
+  console.log(msg);
+	bot.sendMessage(msg.chat.id, "Message logged to console!")
+})
